edef2a55ed12778346f5b7d21f77db9e1d65afa5
Andres Escobar <Andres.Escobar@aexp.com>
2017-01-20 16:07:30 -0700

    test(jestPreset): fixed linting issues

--- __tests__/jest-preset.spec.js
+++ __tests__/jest-preset.spec.js
@@ -2,11 +2,11 @@ describe('jest-preset.json', () => {
   it('should be valid parseable json', () => {
     let isValidJson = true;
     try {
-      const jestPreset = require('../jest-preset.json');
+      require('../jest-preset.json');
     } catch (error) {
       isValidJson = false;
     }
-    // console.log(jestPreset);
+
     expect(isValidJson).toBe(true);
   });
 });
