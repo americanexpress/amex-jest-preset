d3c79c7a23a1d3088b92878fe6c1f22888daf94f
Andres Escobar <Andres.Escobar@aexp.com>
2017-02-13 08:53:33 -0700

    chore(package): adding one-amex-jest-preset to jest config

--- jest-setup.js
+++ jest-setup.js
@@ -1,7 +1,10 @@
 // thanks to jest
 // this is a dev-time file
+// ignoring this for istanbul because it is config file that is likely temp and to be replaced with https://www.npmjs.com/package/jest-junit-reporter
+/* istanbul ignore next */
 const jasmineReporters = require('jasmine-reporters');
 
+/* istanbul ignore next */
 jasmine.getEnv().addReporter(
   new jasmineReporters.JUnitXmlReporter({
     consolidateAll: true,
