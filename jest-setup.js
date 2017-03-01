1a3b4b8668af075a2966bbe02b878c0dbec1adbb
Andres Escobar <Andres.Escobar@aexp.com>
2017-03-01 11:11:42 -0700

    chore(package): adding coverageIgnorePatterns to ignore jest-setup.js

--- jest-setup.js
+++ jest-setup.js
@@ -1,15 +1,11 @@
 // thanks to jest
 // this is a dev-time file
 // ignoring this for istanbul because it is config file that is likely temp and to be replaced with https://www.npmjs.com/package/jest-junit-reporter
-/* istanbul ignore next */
 const jasmineReporters = require('jasmine-reporters');
-/* istanbul ignore next */
 const matchers = require('one-amex-test-utils').matchers;
 
-/* istanbul ignore next */
 expect.extend(matchers);
 
-/* istanbul ignore next */
 jasmine.getEnv().addReporter(
   new jasmineReporters.JUnitXmlReporter({
     consolidateAll: true,
