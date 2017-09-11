25e03959ef23d408385b1035ef0ed7dfb2799006
Andres Escobar <Andres.Escobar@aexp.com>
2017-09-11 13:32:00 -0700

    feat(preset): add cache directory

--- create-jest-preset.js
+++ create-jest-preset.js
@@ -28,6 +28,7 @@ const jestPreset = {
     '!lib/**',
     '!test-results/**',
   ],
+  cacheDirectory: '<rootDir>/test-results/cache',
   coverageDirectory: '<rootDir>/test-results/coverage',
   coverageReporters: [
     'text',
